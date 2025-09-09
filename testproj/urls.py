53437fbad8f434bb708b7a4682805feed0b17ad0
alam51 <jccbuet1@gmail.com>
2025-09-09 20:27:05 +0600

    .

--- testproj/urls.py
+++ testproj/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("", include("pwa.urls", namespace="pwa")),
     path("", include("pwa.urls")),
-    path("", include("testproj.core.urls", namespace="core")),
+    path("", include("testproj.core.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
